44e60791d1adf8e9c32fe2cd882514a42a7586d5
Leandro Schuab <lschuab@gmail.com>
2018-10-10 09:19:05 -0700

    testing routes and split login

--- config/routes.js
+++ config/routes.js
@@ -2,14 +2,21 @@
 const knex = require("../db/knex.js");
 
 const users = require("../controllers/users.js");
+const developers = require("../controllers/developers.js");
+const businesses = require("../controllers/businesses.js");
 
 
 module.exports = function(app){
   app.use(createErrorArr);
 
   app.get('/', users.index);
-  app.post('/register', users.register);
-  app.post('/login', users.login);
+  app.get('/developers/login', developers.loginForm);
+  app.get('/businesses/login', businesses.loginForm);
+
+  app.get('/business', (req, res) => res.render('business'));
+  app.get('/newproject', (req, res) => res.render('new_project'));
+  app.get('/projectinfo', (req, res) => res.render('project_info'));
+
 
 
   app.use(authenticateUser);
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -23,27 +23,3 @@ registerForm.addEventListener('submit', (e) => {
     return true;
   }
 });
-
-function adaptRegistration() {
-  const roleSelect = document.getElementById('role-select');
-  const businessFields = document.getElementById('business-fields');
-  if (roleSelect.value === "business") {
-    const businessFields = document.getElementById('business-fields');
-    businessFields.innerHTML = `
-    <div class="form-group">
-      <label for="company-name-input">Company Name</label>
-      <input type="text" class="form-control" id="company-name-input" placeholder="Company Name" name="company_name" required>
-    </div>
-    <div class="form-group">
-      <label for="contact-number-input">Contact Number</label>
-      <input type="tel" class="form-control" id="contact-number-input" placeholder="Contact Number" name="phone_number">
-    </div>
-    <div class="form-group">
-      <label for="company-url-input">Company URL</label>
-      <input type="text" class="form-control" id="company-url-input" placeholder="Company URL" name="company_url">
-    </div>
-    `;
-  } else {
-    businessFields.innerHTML = ``;
-  }
-}
